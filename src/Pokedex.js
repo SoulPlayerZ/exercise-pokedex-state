2a0c5eeb5042f2376484dc9f1ee398d3d7735dd3
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-02 22:05:29 -0300

    Pokedex - funçao direto no pokemon. Botao none

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import pokemons from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor(){
+      super();
+      this.state = {
+        position: 0
+      };
+      this.nextPokemon = this.nextPokemon.bind(this);
+    } 
+    nextPokemon(){  
+       if (this.state.position !== pokemons.length - 1) {
+        this.setState((prevState, _props) => ({
+                position: (prevState.position + 1)
+           }))
+        }else{
+         this.setState(({
+                position: 0
+           }))
+        }
+        return this.state.position;
+   
+    
+    }
     render() {
         return (
+            // se for fazer a questão 2 nenem. Volta com o botão
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+              <div className='poke' onClick={this.nextPokemon}>
+                 {<Pokemon key={pokemons[this.state.position].id} pokemon={pokemons[this.state.position]} />}
+                 <button className='poke-btn' >Next</button>
+              </div>
+                
             </div>
         );
     }
