c93cd8f472184fd1b3973afd1ffc956408e3e710
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-03 21:35:13 -0300

    Deixando formulario montado feio para estudar no Sabado e melhorar

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,6 +2,7 @@ import React from 'react';
 import pokemons from './data';
 import Pokemon from './Pokemon';
 
+
 class Pokedex extends React.Component {
     constructor(){
       super();
@@ -10,27 +11,15 @@ class Pokedex extends React.Component {
       };
       this.nextPokemon = this.nextPokemon.bind(this);
     } 
-    nextPokemon(){  
-       if (this.state.position !== pokemons.length - 1) {
-        this.setState((prevState, _props) => ({
-                position: (prevState.position + 1)
-           }))
-        }else{
-         this.setState(({
-                position: 0
-           }))
-        }
-        return this.state.position;
    
-    
-    }
     render() {
         return (
             // se for fazer a questão 2 nenem. Volta com o botão
             <div className="pokedex">
-              <div className='poke' onClick={this.nextPokemon}>
+              <div className='poke'>
                  {<Pokemon key={pokemons[this.state.position].id} pokemon={pokemons[this.state.position]} />}
-                 <button className='poke-btn' >Next</button>
+                 {/* {pokemons.map((poke) => <Pokemon key={poke.id} pokemon={poke}/>)} */} 
+                 <button className='poke-btn' onClick={this.nextPokemon}>Next</button>
               </div>
                 
             </div>
